ab142bd85265f07a40dfbce6b74c2b62bebcdb59
oscarbns <34193513+oscarbns@users.noreply.github.com>
2019-04-19 15:46:13 +0200

    plataformas

---
+++ Plataformas 3d/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour {
+
+    public GameObject[] waypoints;
+    public GameObject player;
+    int current = 0;
+    public float speed;
+    float WPradius = 1;
+
+    void Update()
+    {
+        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        {
+            current++;
+            if (current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject == player)
+        {
+            player.transform.parent = transform;
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject == player)
+        {
+            player.transform.parent = null;
+        }
+    }
+}
